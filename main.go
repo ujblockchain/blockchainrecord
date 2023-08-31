75cf7090fc949c7ce5095d37a9f977238e880e22
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-31 12:28:29 +0200

    render assets & templates and load routers

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	asset "github.com/ujblockchain/blockchainrecord/constants"
+	"github.com/ujblockchain/blockchainrecord/router"
 	"github.com/ujblockchain/blockchainrecord/server"
 )
 
 func main() {
 	//init echo
-	router := echo.New()
+	app := echo.New()
 
-	router.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	//render template path
+	app.Renderer = asset.LoadTemplates()
+
+	// load static asset
+	asset.LoadAssets(app)
+
+	//load router
+	router.LoadRouters(app)
 
 	// listen and serve on 0.0.0.0:8080
-	server.RunServer(router)
+	server.RunServer(app)
 }
